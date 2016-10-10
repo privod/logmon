2827401c3881e4d2a46c952de3dc8f8c439ba0f4
Bespalov Nikolay <bespalov@krsk.ustech.ru>
2016-10-10 15:03:18 +0700

    Тесты отвязвны от внешних данных

--- tests.py
+++ tests.py
@@ -1,17 +1,19 @@
 from unittest import TestCase
-
 from datetime import datetime
 from logmon.log import Parser, Level
 
 
 class LogTestCase(TestCase):
     def test_parser(self):
-        with open('logmon/testdata/test.log', 'rb') as file:
-            log_bytes = file.read()
 
-            log_list = Parser(Level.INFO).pars(log_bytes, datetime(2016, 6, 24, 12, 30))
-            self.assertEqual(log_list[0].get_text(), 'i = 160	yuid.uid = 8748	yuid.type_letter = 0')
+        log_bytes = """I 2016.06.24 12:06:29.504: xml.mread_blob...........end
+I 2016.06.24 12:06:38.159: i = 160	yuid.uid = 8748	yuid.type_letter = 0
+I 2016.06.24 12:06:38.284: AD230095@ncr.com
+I 2016.06.24 12:06:38.299: NCR Reports...""".encode()
 
-            # log_list = Parser(Level.INFO).pars(log_bytes, datetime(2016, 6, 24, 12, 59, 59))
-            # for log in log_list:
-            #     print(log)
+        log_list = Parser(Level.INFO).pars(log_bytes, datetime(2016, 6, 24, 12, 6, 30))
+        self.assertEqual(log_list[0].get_text(), 'i = 160	yuid.uid = 8748	yuid.type_letter = 0')
+
+        # log_list = Parser(Level.INFO).pars(log_bytes, datetime(2016, 6, 24, 12, 59, 59))
+        # for log in log_list:
+        #     print(log)
